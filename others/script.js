e14d7a739f967748bdff6f7fdd6d664be1470380
pachakutiq199999 <eddyauyeung+github@disroot.org>
2022-05-31 00:04:45 +0800

    fix responsive voice

--- others/script.js
+++ others/script.js
@@ -3,12 +3,14 @@ var read = [].slice.call(document.querySelectorAll('use'));
 const text = ['singing', 'painting', 'photographing', 'fishing', 'reading', 'watching TV', 'watching movie', 'What is your hobby?', 'My hobby is fishing.', 'playing TV games', 'playing yoga', 'playing chess', 'playing cards', 'playing puzzles', 'playing the piano', 'playing the guitar', 'playing the violin', 'What is your hobby?', 'Playing yoga is one of my hobbies.']
 
 function speakfirst() {
-    responsiveVoice.speak("cooking", { rate: 0.5 });
+    responsiveVoice.setDefaultRate(0.5);
+    responsiveVoice.speak("cooking");
 }
 
 read.forEach(function (content, index) {
     content.addEventListener('click', function () {
+        responsiveVoice.setDefaultRate(0.5);
         var speaktext = text.at(index);
-        responsiveVoice.speak(speaktext, { rate: 0.5 });
+        responsiveVoice.speak(speaktext);
     });
 });
