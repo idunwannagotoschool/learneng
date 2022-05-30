882e266425ec206b76530743cecd91f2c6f2c8b9
pachakutiq199999 <eddyauyeung+github@disroot.org>
2022-05-30 23:47:17 +0800

    update others

--- others/script.js
+++ others/script.js
@@ -1,6 +1,6 @@
 var read = [].slice.call(document.querySelectorAll('use'));
 
-const text = ['singing', 'painting', 'photographing', 'fishing', 'reading', 'watching TV', 'watching movie', 'What is your hobby?', 'My hobby is fishing.']
+const text = ['singing', 'painting', 'photographing', 'fishing', 'reading', 'watching TV', 'watching movie', 'What is your hobby?', 'My hobby is fishing.', 'playing TV games', 'playing yoga', 'playing chess', 'playing cards', 'playing puzzles', 'playing the piano', 'playing the guitar', 'playing the violin', 'What is your hobby?', 'Playing yoga is one of my hobbies.']
 
 function speakfirst() {
     responsiveVoice.speak("cooking");
