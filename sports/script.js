7fb02ad595cc49041710e39b353b1146de749301
pachakutiq199999 <eddyauyeung+github@disroot.org>
2022-05-30 23:57:13 +0800

    update

--- sports/script.js
+++ sports/script.js
@@ -3,12 +3,12 @@ var read = [].slice.call(document.querySelectorAll('use'));
 const text = ['singing', 'painting', 'photographing', 'fishing', 'reading', 'watching TV', 'watching movie', 'What is your hobby?', 'My hobby is fishing.', 'playing TV games', 'playing yoga', 'playing chess', 'playing cards', 'playing puzzles', 'playing the piano', 'playing the guitar', 'playing the violin', 'What is your hobby?', 'Playing yoga is one of my hobbies.']
 
 function speakfirst() {
-    responsiveVoice.speak("cooking", { rate: 0.5 });
+    responsiveVoice.speak("cooking");
 }
 
 read.forEach(function (content, index) {
     content.addEventListener('click', function () {
         var speaktext = text.at(index);
-        responsiveVoice.speak(speaktext, { rate: 0.5 });
+        responsiveVoice.speak(speaktext);
     });
 });
