562adff755e0ee6c13898e72a9920c922c94bb9e
pachakutiq199999 <eddyauyeung+github@disroot.org>
2022-05-31 15:17:00 +0800

    update sentences

--- sentences/script.js
+++ sentences/script.js
@@ -1,13 +1,13 @@
 var read = [].slice.call(document.querySelectorAll('use'));
 
-const text = ['singing', 'painting', 'photographing', 'fishing', 'reading', 'watching TV', 'watching movie', 'What is your hobby?', 'My hobby is fishing.', 'playing TV games', 'playing yoga', 'playing chess', 'playing cards', 'playing puzzles', 'playing the piano', 'playing the guitar', 'playing the violin', 'What is your hobby?', 'Playing yoga is one of my hobbies.']
+const text = ["I'm fine, thanks.", 'Hello, how are you doing?', 'Fine, thank you.', "Let me introduce myself.", "May I introduce myself?", "Let me introduce him to you.", "May I introduce her to you?", "May I introduce a friend of mine to you?", "May I introduce my colleague? This is Mary.", 'See you later.', 'See you tomorrow.', 'Have a good evening.', 'Have a nice day.', "It's nice to meet you.", "Nice to meet you, too."]
 
 var rate = document.querySelector('#rate');
 var ratevalue = document.querySelector('#rate-value');
 
 function speakfirst() {
     responsiveVoice.setDefaultRate(rate.value);
-    responsiveVoice.speak("cooking");
+    responsiveVoice.speak("Hi, how are you?");
 }
 
 read.forEach(function (content, index) {
